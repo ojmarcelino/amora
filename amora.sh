971cbcf59a99b464993c58b2440efee3b24ab55d
Juliano Marcelino <14352515+ojmarcelino@users.noreply.github.com>
2021-09-01 23:19:58 -0300

    Create amora.sh

---
+++ amora.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# amora.sh - Awesome MObile Remote Access
+#
+# Site      : https://github.com/ojmarcelino/amora
+# Author    : ojmarcelino
+#
+#  ------------------------------------------------------------
+#  This script uses scrcpy utility to access a mobile device,
+#  but do some useful things before the scrcpy command.
+#  ------------------------------------------------------------
+#
+#
+# History:
+#
+#    v0.1 2021-09-01:
+#       - really, just an empty bash, without comments and just scrcpy
+scrcpy
